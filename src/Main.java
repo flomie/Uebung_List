b5f226f7d83a3e1988277411642d60b16481113d
SneakyZippy <flex.zoeggeler@live.de>
2024-11-14 12:39:33 +0100

    MeineÄnderung dass i stink

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Patient p = new Patient ("F001", 123456, "Angela", "Müller", LocalDate.of(1989,02, 05));
-        Pateint p1 = new Patient("F003", 345678, "Angela", "Müller", LocalDate.of(1998,07,07));
+        Patient p1 = new Patient("F003", 345678, "Angela", "Müller", LocalDate.of(1998,07,07));
         Patient p2 = new Patient("F002", 789012, "Bertram", "Müller", LocalDate.of(1990,8,15));
 
         System.out.println("sippyStinkt");
